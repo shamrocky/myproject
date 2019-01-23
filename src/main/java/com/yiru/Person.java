47b179332b6946bd3978168e9666624e79ec48d1
unknown <yiru.chen@synbiotech.com.tw>
2019-01-23 15:41:58 +0800

    Initial commit

---
+++ src/main/java/com/yiru/Person.java
@@ -0,0 +1,9 @@
+package com.yiru;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello Java");
+
+    }
+
+}
---
+++ src/main/kotlin/com/yiru/Hello.kt
@@ -0,0 +1,14 @@
+package com.yiru
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+//    Human().Hello()
+    val h=Human()
+    h.Hello()
+}
+
+class Human{
+    fun Hello(){
+        println("Hello Kotlin.")
+    }
+}
