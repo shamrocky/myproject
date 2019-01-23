e45708e68387f95ef4811d2c1cdf8fe8a07c1706
yiru.chen <shamrocky1984@gmail.com>
2019-01-23 17:14:22 +0800

    Java and Kotlin Variable, data tpye

--- src/main/java/com/yiru/Hello.java
+++ src/main/java/com/yiru/Hello.java
@@ -1,10 +1,22 @@
 package com.yiru;
 
+import java.util.concurrent.SynchronousQueue;
+
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello World");
 //        new Person().hello();
         Person p=new Person();
         p.hello();
+        int age=19;
+        Integer age2=15;
+        char c='我';
+        Character c2=c;
+        System.out.println(c2);
+        byte b=120;
+        float weight=66.5f;
+        System.out.println(age);
+        String name="Yiru";
+
     }
 }
--- src/main/kotlin/com/yiru/Hello.kt
+++ src/main/kotlin/com/yiru/Hello.kt
@@ -5,6 +5,13 @@ fun main(args: Array<String>) {
 //    Human().Hello()
     val h=Human()
     h.Hello()
+    val age : Int=19
+    var age2 : Int=20
+    age2=21
+    var weight=66.5f
+    val name : String
+    name="Yiru"
+    println(name)
 }
 
 class Human{
